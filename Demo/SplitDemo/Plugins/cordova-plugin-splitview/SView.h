8e24606e9371a101b2f614f1ed9703110a12b3fe
j-crosson <67383519+j-crosson@users.noreply.github.com>
2020-12-22 11:21:20 -0500

    Add files via upload

---
+++ Demo/SplitDemo/Plugins/cordova-plugin-splitview/SView.h
@@ -0,0 +1,19 @@
+//
+//  SView.h
+//  splitios
+//
+//  Created by jerry on 6/2/20.
+//
+
+#ifndef SView_h
+#define SView_h
+
+
+@interface CDVViewController (t)
+
+@property (nonatomic, readwrite, strong) UIView* launchView;
+
+
+@end
+
+#endif /* SView_h */
